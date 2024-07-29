796cff52950382652243c9b72acd22ff5ab70938
AstronoMoore <tom.moore017@gmail.com>
2024-07-29 09:37:04 -0400

    adding to pypi

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ setup_requires=['setuptools_scm'],
 
 setup(
     name='vogon',
+    version='0.1.0',
+    author='Thomas Moore',
+    author_email='tom.moore017@gmail.com',
+    description='A basic data aggregator for astrophysical transients',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     packages=find_packages(),
+    python_requires='>=3.7',
     install_requires=[
         'pandas',
         'astropy',
