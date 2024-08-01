6561b871b2b9f756ccaf3ae172ddcc63d46966f8
AstronoMoore <tom.moore017@gmail.com>
2024-08-01 11:01:50 -0400

    fixing settings template imports

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup_requires=['setuptools_scm'],
 
 setup(
     name='vogon',
-    version='0.1.3.1',
+    version='0.1.3.2',
     author='Thomas Moore',
     author_email='tmoore11@qub.ac.uk',
     description='A basic data aggregator for astrophysical transients',
